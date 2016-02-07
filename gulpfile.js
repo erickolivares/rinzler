264fedc5d07502008d19df44fc8de64891c377f4
Erick Olivares <erickolivares21@gmail.com>
2016-02-07 16:22:11 -0700

    Fixed SASS compiler bug, Added livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,24 @@
 var gulp = require('gulp'),
 	sass = require('gulp-sass'),
-	sourcemaps = require('gulp-sourcemaps');
+	sourcemaps = require('gulp-sourcemaps'),
+  livereload = require('gulp-livereload');
 
-var input = './scss/**/style.scss',
-	output = './css';
+var input = './scss/style.scss',
+	output = './css/';
 
 gulp.task('sass', function () {
   return gulp.src(input)
     .pipe(sourcemaps.init())
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest(output));
+    .pipe(gulp.dest(output))
+    .pipe(livereload({ start: true }));
 });
 
 gulp.task('watch', function() {
+  livereload.listen();
   return gulp
-    .watch(input, ['sass'])
+    .watch('./scss/**/*.scss', ['sass'])
     .on('change', function(event) {
     });
 });
