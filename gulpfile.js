7de7163f8abd150869f03fa0d54fd96f3b68331b
Erick Olivares <erickolivares21@gmail.com>
2017-04-17 15:33:57 -0700

    added gulp minify js, added columns to sass, now minified css and html are stored in build

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,23 @@
 var gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	sourcemaps = require('gulp-sourcemaps'),
-  livereload = require('gulp-livereload');
+    livereload = require('gulp-livereload');
 
 var input = './scss/style.scss',
 	output = './css/';
 
+var htmlInput = 'index.html',
+    htmlOutput = './build/',
+    htmlmin = require('gulp-htmlmin');
+
+var minify = require('gulp-minify'),
+    jsOutput = './js';
+
+
 gulp.task('sass', function () {
   return gulp.src(input)
     .pipe(sourcemaps.init())
-    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(sass())
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(output))
     .pipe(livereload({ start: true }));
@@ -23,4 +31,30 @@ gulp.task('watch', function() {
     });
 });
 
-gulp.task('run', ['watch', 'sass']);
+gulp.task('compress', function() {
+  gulp.src('./js/*.js')
+    .pipe(minify({
+        ext:{
+            min:'.js'
+        },
+        exclude: ['tasks'],
+        ignoreFiles: ['.combo.js', '-min.js']
+    }))
+    .pipe(gulp.dest(jsOutput));
+});
+
+gulp.task('minifycss', function () {
+  return gulp.src(input)
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(gulp.dest('./build/css/'))
+});
+
+gulp.task('minify', function() {
+  return gulp.src(htmlInput)
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest(htmlOutput));
+});
+
+
+gulp.task('run', ['watch', 'sass','compress']);
+gulp.task('production', ['minifycss', 'minify']);
