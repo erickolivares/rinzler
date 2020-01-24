89b871690c4efc802887c812f2e2c6fe9c2f6fa6
Erick Olivares <erick@turn7.io>
2020-01-24 17:24:31 -0600

    added fullscreen capability

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@ $(function(){
 	**/
 
     $(".menu-toggle").on('click', function() {
-
         $(this).toggleClass("on");
         $('body').toggleClass("menu-visible");
         if ($('body').hasClass("menu-visible")) {
@@ -15,7 +14,33 @@ $(function(){
         } else {
             $(".menu-main-menu-container").slideUp();
         }
-        
     });  
+
+    /**
+    ** 
+    ** Fill Screen
+    **
+    **/    
+
+
+    function fullScreen() {
+        var fullscreen = $('.fullscreen');
+        //Offset
+        // var offset = 0;
+        // var offset = fullscreen.attr('height-offset');
+        //WordPress 
+        if($('body').hasClass('logged-in')) {
+            var wpAdminHeight = $('#wpadminbar').outerHeight();
+            offset = offset + wpAdminHeight;
+        }
+        var newheight = $(window).height();      
+        fullscreen.css('height',newheight);
+    }
+
+    fullScreen();
+
+   $(window).resize(function() {
+        fullScreen();
+    });        
 
 });
